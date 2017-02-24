c1f2e7a8396a9ef6d5c124d2b0cb1556662e092c
Kristina Martinez <misskristinamartinez@gmail.com>
2017-02-24 06:23:34 -0500

    get request working

---
+++ quizRouter.js
@@ -0,0 +1,29 @@
+const router = express.Router();
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating quiz item \`${req.params.id}\``);
+   const updatedItem = Quiz.update({
+    id: req.params.id,
+    name: req.body.name,
+    checked: req.body.checked
+  });
+  res.json(updatedItem);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+const Quiz = require('./models');
+//const quizRouter = require('./quizRouter');
+//app.use('/quizzes', quizRouter);
 
+
+Quiz.create('How to Train a Dragon');
+Quiz.create('How to Succeed in Business Without Really Trying');
+Quiz.create('How the West Was Won');
+
+
+app.get('/quizzes', (req, res) => {
+  res.json(Quiz.get());
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
