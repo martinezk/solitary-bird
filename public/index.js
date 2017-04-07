9c9e3850194405b49f9da0b4aa466c4787f3cb9b
Kristina Martinez <misskristinamartinez@gmail.com>
2017-04-07 07:00:24 -0400

    using eslint

--- public/index.js
+++ public/index.js
@@ -5,116 +5,115 @@ var currentPage = 1;
 
 //state modification functions
 //--if an answer is chosen, add to quiz total
-function userChoice(selectedAns){	
-	quizTotal+=selectedAns;
-	console.log(quizTotal);
+function userChoice(selectedAns) {
+    quizTotal += selectedAns;
+    console.log(quizTotal);
 }
 
 //render functions
-
-function displayScore(){
-	if (currentPage > 3){
-		$('.score').text(quizTotal);
-		$('#start-over').removeClass('hidden');
-		$('.finalscore').removeClass('hidden');
-		$('#next').addClass('hidden');
-	}
+function displayScore() {
+    if (currentPage > 3) {
+        $('.score').text(quizTotal);
+        $('#start-over').removeClass('hidden');
+        $('.finalscore').removeClass('hidden');
+        $('#next').addClass('hidden');
+    }
 }
 
 var QUIZZES_URL = '/quizzes';
 var QUIZZES = [];
 
-function displayQuizQuestions(quizIndex){
-	var quiz = QUIZZES[quizIndex];
-	for(var i = 0; i < quiz.questions.length; i++) {
-		var question = quiz.questions[i];
-		var html = createQuestion(question, i);
-		$('.quiz').append(html);
-	}
-	$('.questions' ).first().toggleClass('hidden').addClass('show');
-	$('#next').removeClass('hidden');
-	hideQuizChoices();
-	$('.quiz').removeClass('center');
+function displayQuizQuestions(quizIndex) {
+    var quiz = QUIZZES[quizIndex];
+    for (var i = 0; i < quiz.questions.length; i++) {
+        var question = quiz.questions[i];
+        var html = createQuestion(question, i);
+        $('.quiz').append(html);
+    }
+    $('.questions').first().toggleClass('hidden').addClass('show');
+    $('#next').removeClass('hidden');
+    hideQuizChoices();
+    $('.quiz').removeClass('center');
 };
 
-function getQuizzes(){
-	var settings = {
-		url: QUIZZES_URL,
-		data: {},
-		dataType: 'json',
-		type: 'GET',
-		success: function(json){
-			QUIZZES = json;
-			displayQuizzes(QUIZZES);
-		},
-		error: function(){
-			alert("Something went wrong. Please refresh the page and try again.	")
-		}
-	};
-	$.ajax(settings);
+function getQuizzes() {
+    var settings = {
+        url: QUIZZES_URL,
+        data: {},
+        dataType: 'json',
+        type: 'GET',
+        success: function (json) {
+            QUIZZES = json;
+            displayQuizzes(QUIZZES);
+        },
+        error: function () {
+            alert("Something went wrong. Please refresh the page and try again.    ")
+        }
+    };
+    $.ajax(settings);
 }
 
-function hideQuizChoices(){	
-	$('.quiz-choices').toggleClass("hidden");
-}
-	
-function displayQuizzes(quizzes){
-	var quizChoices = "";
-	for (var i=0; i<quizzes.length; i++){
-		quizChoices += `
-		<button class= "begin inline" type="submit" onclick="displayQuizQuestions(${i})"><h3>${quizzes[i].name}</h3></button>`;
-	}
-	var html = `
-		<div class="quiz-choices">
-			${quizChoices}
-		</div>
-	`;
-	$('.quiz').append(html);
+function hideQuizChoices() {
+    $('.quiz-choices').toggleClass("hidden");
 }
 
-function createQuestion(questionJson, index){
-	return `
-		<div class="questions hidden">
-			<h2 class="quiz-title"> ${questionJson.question} </h2>
-			<ul>	
-				<li class= "answers"><input type="radio" name="ans-${index}" value="1"><label>${questionJson.answers[0]}</label></li>
-				<li class= "answers"><input type="radio" name="ans-${index}" value="2"><label>${questionJson.answers[1]}</label></li>
-				<li class= "answers"><input type="radio" name="ans-${index}" value="3"><label>${questionJson.answers[2]}</label></li>
-				<li class= "answers"><input type="radio" name="ans-${index}" value="4"><label>${questionJson.answers[3]}</label></li>
-			</ul>
-			<p> ${index+=1} of 3</p>
-		</div>
-	`;
+function displayQuizzes(quizzes) {
+    var quizChoices = "";
+    for (var i = 0; i < quizzes.length; i++) {
+        quizChoices += `
+        <button class= "begin inline" type="submit" onclick="displayQuizQuestions(${i})"><h3>${quizzes[i].name}</h3></button>`;
+    }
+    var html = `
+        <div class="quiz-choices">
+            ${quizChoices}
+        </div>
+    `;
+    $('.quiz').append(html);
 }
 
-function resetQuizzes(){
-	quizTotal = 0;
-		currentPage = 1;
-		$('.questions').remove();
-		$('.finalscore').removeClass('show').addClass('hidden');
-		$('.quiz-choices').removeClass('hidden');
-		$('#start-over').addClass('hidden');
-		$('#next').addClass('hidden');
+function createQuestion(questionJson, index) {
+    return `
+        <div class="questions hidden">
+            <h2 class="quiz-title"> ${questionJson.question} </h2>
+            <ul>    
+                <li class= "answers"><input type="radio" name="ans-${index}" value="1"><label>${questionJson.answers[0]}</label></li>
+                <li class= "answers"><input type="radio" name="ans-${index}" value="2"><label>${questionJson.answers[1]}</label></li>
+                <li class= "answers"><input type="radio" name="ans-${index}" value="3"><label>${questionJson.answers[2]}</label></li>
+                <li class= "answers"><input type="radio" name="ans-${index}" value="4"><label>${questionJson.answers[3]}</label></li>
+            </ul>
+            <p> ${index += 1} of 3</p>
+        </div>
+    `;
 }
 
-$(document).ready(function() {
-	getQuizzes();
-	$('#next').click(function(){
-		var value = $('.show input:checked').val();
-		if(value === undefined){
-			alert("You haven't chosen one!");
-		} else {
-			value = parseInt(value);
-			userChoice(value);
-			$('.show').removeClass('show').addClass('hidden').next().removeClass('hidden').addClass('show');
-			currentPage++;
-			displayScore();
-		}
-	});
-	$('#start-over').click(resetQuizzes);
-	$('#nav-quizzes').click(resetQuizzes);
-	$('#header__icon').click(function(){
-		$('.links').toggleClass('hidden');
-	})
+function resetQuizzes() {
+    quizTotal = 0;
+    currentPage = 1;
+    $('.questions').remove();
+    $('.finalscore').removeClass('show').addClass('hidden');
+    $('.quiz-choices').removeClass('hidden');
+    $('#start-over').addClass('hidden');
+    $('#next').addClass('hidden');
+}
+
+$(document).ready(function () {
+    getQuizzes();
+    $('#next').click(function () {
+        var value = $('.show input:checked').val();
+        if (value === undefined) {
+            alert("You haven't chosen one!");
+        } else {
+            value = parseInt(value);
+            userChoice(value);
+            $('.show').removeClass('show').addClass('hidden').next().removeClass('hidden').addClass('show');
+            currentPage++;
+            displayScore();
+        }
+    });
+    $('#start-over').click(resetQuizzes);
+    $('#nav-quizzes').click(resetQuizzes);
+    $('#header__icon').click(function () {
+        $('.links').toggleClass('hidden');
+    })
 })
 
--- test/test-server.js
+++ test/test-server.js
@@ -8,83 +8,84 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const newQuiz =
-	{
-		"name": "How to Train Your Dragon",
-		"questions": [{
-			"question": "What is Art for?",
-			"correct": 1,
-			"answers": ["A catharis for our emotions", "To embody the most ethical ideas", "To teach us to be our 'super' selves", "For expressing what words can never fully do"]
-		},
-		{
-			"question": "Which pastime seems appealing to you?",
-			"correct": 1,
-			"answers": ["Backgammon", "Dinner parties", "Reclining in a chair with a glass of milk", "Billards"]
-		},
-		{
-			"question": "What is reason for?",
-			"correct": 1,
-			"answers": ["Reason is a slave to the emotions", "To help us make moral choices", "To give understanding to the emotions", "Reason is faulty. We privilege certain ideas over others"]
-		}]
-	};
+    {
+        "name": "How to Train Your Dragon",
+        "questions": [{
+            "question": "What is Art for?",
+            "correct": 1,
+            "answers": ["A catharis for our emotions", "To embody the most ethical ideas", "To teach us to be our 'super' selves", "For expressing what words can never fully do"]
+        },
+        {
+            "question": "Which pastime seems appealing to you?",
+            "correct": 1,
+            "answers": ["Backgammon", "Dinner parties", "Reclining in a chair with a glass of milk", "Billards"]
+        },
+        {
+            "question": "What is reason for?",
+            "correct": 1,
+            "answers": ["Reason is a slave to the emotions", "To help us make moral choices", "To give understanding to the emotions", "Reason is faulty. We privilege certain ideas over others"]
+        }]
+    };
 
 describe('Quizzes', function () {
-	before(function () {
-		return runServer();
-	});
+    before(function () {
+        return runServer();
+    });
 
-	after(function () {
-		return closeServer();
-	});
+    after(function () {
+        return closeServer();
+    });
 
-	it('should list available quizzes on GET', function () {
-		return chai.request(app)
-			.get('/quizzes')
-			.then(function (res) {
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.a('array');
-				res.body.length.should.be.at.least(1);
-				const expectedKeys = ['name', 'questions'];
-				res.body.forEach(function (item) {
-					item.should.be.a('object');
-					item.should.include.keys(expectedKeys);
-				});
-			});
-	});
+    it('should list available quizzes on GET', function () {
+        return chai.request(app)
+            .get('/quizzes')
+            .then(function (res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.be.at.least(1);
+                const expectedKeys = ['name', 'questions'];
+                res.body.forEach(function (item) {
+                    item.should.be.a('object');
+                    item.should.include.keys(expectedKeys);
+                });
+            });
+    });
 
-	it('should add a quiz on POST', function () {
-		return chai.request(app)
-			.post('/quizzes')
-			.send(newQuiz)
-			.set('content-type', 'application/json')
-			.then(function (res) {
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.a('object');
-				res.body.should.include.keys('__v', '_id', 'name', 'questions');
-				res.body.name.should.equal(newQuiz.name);
-				res.body.questions.should.be.a('array');
-				res.body._id.should.not.be.null;
+    it('should add a quiz on POST', function () {
+        return chai.request(app)
+            .post('/quizzes')
+            .send(newQuiz)
+            .set('content-type', 'application/json')
+            .then(function (res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.include.keys('__v', '_id', 'name', 'questions');
+                res.body.name.should.equal(newQuiz.name);
+                res.body.questions.should.be.a('array');
+                res.body._id.should.not.be.null;
 
-				//assigning id to our test quiz
-				newQuiz.id = res.body._id;
-			});
-	});
-		it('should edit a quiz on PUT', function () {
-			newQuiz.name = "What Philosopher are You?";
-			return chai.request(app)
-				.put(`/quizzes/${newQuiz.id}`)
-				.set('content-type', 'application/json')
-				.send(newQuiz)
-			    .then(function (res) {
-					res.should.have.status(204);
-				});
-		});
-		it('should delete a quiz on DELETE', function () {
-			return chai.request(app)
-				.delete(`/quizzes/${newQuiz.id}`)
-			    .then(function (res) {
-					res.should.have.status(204);
-				});
-		});
+                //assigning id to our test quiz
+                newQuiz.id = res.body._id;
+            });
+    });
+
+    it('should edit a quiz on PUT', function () {
+        newQuiz.name = "What Philosopher are You?";
+        return chai.request(app)
+            .put(`/quizzes/${newQuiz.id}`)
+            .set('content-type', 'application/json')
+            .send(newQuiz)
+            .then(function (res) {
+                res.should.have.status(204);
+            });
+    });
+    it('should delete a quiz on DELETE', function () {
+        return chai.request(app)
+            .delete(`/quizzes/${newQuiz.id}`)
+            .then(function (res) {
+                res.should.have.status(204);
+            });
+    });
 });
